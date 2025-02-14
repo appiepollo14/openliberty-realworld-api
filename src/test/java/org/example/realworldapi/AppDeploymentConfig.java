d2966153a3563f808fccce99a4f6bd1cf7b047b5
appiepollo14 <asjer94@live.com>
2025-02-14 09:07:43 +0100

    * Automerge renovate * Updates to postgres in e2e test
    Signed-off-by: appiepollo14 <asjer94@live.com>

--- src/test/java/org/example/realworldapi/AppDeploymentConfig.java
+++ src/test/java/org/example/realworldapi/AppDeploymentConfig.java
@@ -8,8 +8,11 @@ import org.testcontainers.junit.jupiter.Container;
 
 public class AppDeploymentConfig implements SharedContainerConfiguration {
 
+    // renovate: datasource=docker depName=postgres
+    static String postgresVersion = "17.3";
+
     @Container
-    public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16.0").withDatabaseName("postgres_db").withPassword("S3cret").withUsername("postgres_user").withNetworkAliases("testpostgres");
+    public static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:" + postgresVersion).withDatabaseName("postgres_db").withPassword("S3cret").withUsername("postgres_user").withNetworkAliases("testpostgres");
 
     @Container
     public static ApplicationContainer app = new ApplicationContainer().withHttpPort(8080)
